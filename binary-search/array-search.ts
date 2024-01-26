799dc4551818818aa9715b6486c6a4eb0dd0b01b
Alisson Zampietro <alisson.zampietro@gmail.com>
2024-01-26 10:32:42 +0000

    binary search on array

--- binary-search/array-search.ts
+++ binary-search/array-search.ts
@@ -1,4 +1,4 @@
-function binarySearch(arr: [], target: number): string {
+function binarySearch(arr: number[], target: number): string {
     let leftIndex = 0;
     let rightIndex = arr.length - 1;
 
@@ -19,4 +19,6 @@ function binarySearch(arr: [], target: number): string {
 }
 
 
-// let customList = 
+let list = [1,2,3,4,5,6,7,8,9,12,14,15,23,29,50,90];
+
+console.log(binarySearch(list, 14))
